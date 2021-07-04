c4d4dbb7db256de02ad9d6b268fc38527b4acca4
Henrique Sousa <washington.sousa01@fatec.sp.gov.br>
2021-07-04 17:43:24 -0300

    second day

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,39 @@
+// import { useContext } from "react";
+import { Link } from "react-router-dom";
+// import { useAuth } from "../hooks/useAuth";
+
+import illustration from "../assets/images/illustration.svg";
+import logo from "../assets/images/logo.svg";
+import { Button } from "../components/Button";
+
+import "../styles/auth.scss";
+
+export function NewRoom() {
+  // const { user } = useAuth();
+
+  return (
+    <div id="page-auth">
+      <aside>
+        <img
+          src={illustration}
+          alt="Ilustração simbolizando perguntas e respostas"
+        />
+        <strong>Crie salas e Q&amp;A ao-vivo </strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logo} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input type="text" placeholder="Nome da sala" />
+            <Button type="submit">Criar sala</Button>
+          </form>
+          <p>
+            Quer entrar em uma sala existente?<Link to="/">Clique aqui</Link>
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
